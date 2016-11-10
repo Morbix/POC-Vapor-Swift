1819926d8b755e867a38a9e26596643142539f1f
Henrique Morbin <morbin_@hotmail.com>
2016-11-10 10:14:11 -0200

    valid code

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -37,6 +37,7 @@ func getInfo(nodes: [JiNode], index: Int, infoType: InfoType) -> String? {
 }
 
 let correiosUrl = "http://websro.correios.com.br/sro_bin/txect01%24.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI="
+let validCode = "PN287778349BR"
 
 let drop = Droplet()
 
